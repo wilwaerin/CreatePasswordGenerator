ecf636195a90bb70e4bfd65db0ede690d068f5e7
Berkay Mustafaoğlu <79161862+wilwaerin@users.noreply.github.com>
2023-12-04 22:24:06 +0300

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -1,6 +1,3 @@
-# Go to: https://replit.com/@appbrewery/password-generator-start?v=1
-
-#Password Generator Project
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -42,5 +39,3 @@ for char in password:
     passw += char
 
 print(f"Your password is: {passw}")
-
- 
