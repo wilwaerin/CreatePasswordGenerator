6a86dffaf05494d83c0c07c79b69618644cc426f
Berkay Mustafaoğlu <79161862+wilwaerin@users.noreply.github.com>
2023-12-04 22:24:35 +0300

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -6,7 +6,7 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 password=[]
 
-print("Welcome to the PyPassword Generator!")
+print("Welcome to the Password Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
